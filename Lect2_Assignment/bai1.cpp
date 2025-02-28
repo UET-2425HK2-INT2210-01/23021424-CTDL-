d2c118ced8f677675c299dd7609fad35cbadacbf
23021424 <23021424@vnu.edu.vn>
2025-02-28 22:33:33 +0700

    Add files via upload

---
+++ Lect2_Assignment/bai1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream> 
+#include <map>
+#include <vector>
+using namespace std; 
+int main(){
+	map<int, int> f;
+	int n,dem = 0;
+	cin >>n;
+	vector<int>arr(n);
+	for (int i =0; i<n; i++){
+		cin >> arr[i];
+		f[arr[i]]++;
+	}
+	for (const auto& k: f){
+		dem+=k.second*(k.second-1)/2;
+	}
+	cout << dem <<endl;
+	system("pause");
+}
