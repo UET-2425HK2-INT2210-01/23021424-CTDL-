f8b1501ca04281f08f218decedcfb66ea766f618
23021424 <23021424@vnu.edu.vn>
2025-04-04 22:59:29 +0700

    Add files via upload

---
+++ Lect7_Assignments/bai4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string> 
+using namespace std;
+// dùng đệ quy để tạo ra các dãy 0,1 với độ dài là n 
+void generateBinaryRecursive(string prefix, int n) {
+    if (n == 0) { 
+        cout << prefix << endl; 
+        return; 
+    }
+    generateBinaryRecursive(prefix + "0", n - 1); 
+    generateBinaryRecursive(prefix + "1", n - 1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    generateBinaryRecursive("", n);
+    system("pause");
+}
