e96be16f3d971f1a41294491abcd85b7a0f79797
23021424 <23021424@vnu.edu.vn>
2025-04-04 22:59:02 +0700

    Add files via upload

---
+++ Lect7_Assignments/bai3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream> 
+using namespace std; 
+// hàm dùng đệ quy để tính ucln 
+// dùng thuật toán lấy a = b và b=a%b cho đến khi b == 0 thì a chính là ucln
+int ucln (int a, int b){ 
+	if (b == 0 ) return a;
+	else return ucln(b,a%b);
+}
+int main() {
+	int a,b;
+	// nhập 2 số a b 
+	cin >> a >> b;
+	cout << ucln(a,b) <<endl;
+
+}
