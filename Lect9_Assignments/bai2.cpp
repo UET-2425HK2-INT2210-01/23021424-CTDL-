de5d407b29147a81a1bead95fc177c7f62581265
23021424 <23021424@vnu.edu.vn>
2025-05-09 23:10:27 +0700

    Add files via upload

---
+++ Lect9_Assignments/bai2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std; 
+bool findSum(vector<int>& arr, int index, int target, vector<int>& subset) {
+	if (target == 0) return true; // đệ quy dừng khi đã tìm được tổng 
+	if (index >= arr.size() || target < 0) return false; // đệ quy dừng khi đã duyệt tất cả mảng và không tìm được các bộ số có tổng = X 
+
+	//tính tổng bắt đầu từ arr[index]
+	subset.push_back(arr[index]);
+	if (findSum(arr, index + 1, target - arr[index], subset)) return true;
+
+	// tính tổng tiếp theo và bỏ qua arr[index]
+	subset.pop_back();
+	return findSum(arr, index + 1, target, subset);
+}
+
+int main() {
+	int n,x;
+	cin >> n >>x;
+	vector<int>arr(n); 
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	vector<int> subset;
+	if (findSum(arr, 0, x, subset)) cout << "YES" << endl;
+	else cout << "NO" << endl;
+
+}
---
+++ Lect9_Assignments/bai3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std; 
+
+int maxNum(int a, int b) {
+	if (a > b) return a;
+	else return b;
+}
+int main() {
+	int n,m;
+	cin >> n >>m;
+	vector<int> weight(n), value(n);
+	for (int i = 0; i < n; i++) {
+		cin >> weight[i] >> value[i];
+	}
+	vector<int> dp(m + 1, 0);
+	
+	// thuật toán 0/1 Knapsack 
+	for (int i = 0; i < n; i++) {
+		for (int w = m; w >= weight[i]; w--) {
+			dp[w] = maxNum(dp[w], dp[w - weight[i]] + value[i]);
+		}
+	}
+	cout << dp[m] << endl;
+}
